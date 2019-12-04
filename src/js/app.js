1f25abfa6386ca4b51eaad68d5a4a3704f8687ce
Stegura Alex <stegura.alex@gmail.com>
2019-12-05 00:04:14 +0300

    done

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,17 @@
-// TODO: write your code here
-import sum from './basic';
+import findBy from './findBy';
 
-console.log('worked');
+const finder = findBy('name', 'урон');
+const results = [
+  { name: 'маг', type: 'character', description: 'Персонаж, обладающий магическими способностями' },
+  { name: 'заклинание', type: 'attack', description: 'Атака магическим заклинанием' },
+  { name: 'урон', type: 'help', description: 'Страница описания элемента интерфейса' },
+].filter(finder);
 
-console.log(sum([1, 2]));
+console.log(results);
+/*
+[{
+  name: 'урон',
+  type: 'help',
+  description: 'Страница описания элемента интерфейса'
+}]
+*/
